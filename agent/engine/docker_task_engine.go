e45ceaba74f1654c91d49d9f09e1a0049b5db245
Tom Hulihan <hulihan.tom159@gmail.com>
2015-07-29 12:55:42 -0400

    Use cgzip instead of pgzip

--- agent/engine/docker_task_engine.go
+++ agent/engine/docker_task_engine.go
@@ -27,7 +27,8 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	awsS3 "github.com/aws/aws-sdk-go/service/s3"
-	gzip "github.com/klauspost/pgzip"
+
+	gzip "github.com/youtube/vitess/go/cgzip"
 	"golang.org/x/net/context"
 	"strings"
 
